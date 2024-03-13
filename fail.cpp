00b313b341cf1c6ae2d0d671b1236c141f7a7426
mfialaf <56676192+mfialaf@users.noreply.github.com>
2024-03-13 23:02:35 +0100

    Update fail.cpp to hw02

--- fail.cpp
+++ fail.cpp
@@ -1,33 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <iostream>
-int Static[101];
-int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen, 
-	int* arrayHeap, unsigned int arrayHeapLen) {
-  int Stack[101];
 
-  Static[100] = 0; 
-  Stack[100] = 0; 
+void func(float *stck_p, char * heap_p){
+    char *r = (char *)malloc(5 * sizeof(char)); // not dealocated
+    stck_p[5] =  18.2;  // stack out of index
+    stck_p[1] =  '2';  // wrong type assigned
+    heap_p[100] =  '2';  // potentional heap out of index
 
-  for (int i = 0; i <= 5; i++) Stack [i] = 0;
-
-  int* array = new int[5];
-  array[100] = 0; 
-
-  arrayStack[100] = 0; 
-  arrayHeap[100] = 0; 
-
-  for (unsigned int i = 0; i <= arrayStackLen; i++) {
-      arrayStack[i] = 0;
-  }
-  for (unsigned int i = 0; i <= arrayHeapLen; i++) {
-      arrayHeap[i] = 0;
-  }
-
-  return 0;
+    // scanf("%s", r); // load malitious code and rewrite RA + stack overflow
+    strcpy(heap_p, r); // heap overflow
 }
 
-int main(void) {
-  int arrayStack[5];
-  int* arrayHeap = new int[5];
-  memcheckFailDemo(arrayStack, 5, arrayHeap, 5);
-  return 0;
+int main(int argc, char* argv[]) {
+         printf("You have entered %d arguments:\n", argc);
+
+     for (int i = 0; i < argc; i++) {
+         printf("%s\n", argv[i]);
+     }
+    char arg_chr[10];
+    strcpy(arg_chr, argv[1]); // No check on the length of input, stack overflow
+    int arg_num = atoi(argv[2]); // atoi not safe, what is not number
+
+    float f[3] = {3.14, 2.0, 0.66};
+    char *p = (char *)malloc(10 * sizeof(char));
+
+    func(f, p);
+
+    unsigned int n = 4294967295;
+    n += 1; // preteceni na 0
+
+    memcpy(p, arg_chr, 30); // heap overflow, zadna kontrola delky nahravane vs size, kladne cislo
+    free(p); // x
+
+    if (arg_num > 1500) {        
+        int *i = NULL;
+        printf("%d", *i); // using unitialized pointer
+    }
+
+    if (arg_num > 1000) free(p); // potentional double free
+
+    return 0;
 }
